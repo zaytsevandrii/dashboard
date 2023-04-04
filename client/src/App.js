c04ed50533116d224a4d7578fc8d3a8433a2be45
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-04-04 12:13:18 +0100

    add components, layout start and navbar start

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import { CssBaseline, ThemeProvider } from "@mui/material"
 import { createTheme } from "@mui/material/styles"
 import { useMemo } from "react"
 import { useSelector } from "react-redux"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Dashboard from "scenes/dashboard"
+import Layout from "scenes/layout"
 import { themeSettings } from "theme"
 
 function App() {
@@ -9,9 +12,17 @@ function App() {
     const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
     return (
         <div className="App">
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-            </ThemeProvider>
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Routes>
+                        <Route element={<Layout />}>
+                            {/* <Route path="/" element={<Navigate to="dashboard" replace />} /> */}
+                            <Route path="/dashboard" element={<Dashboard/>} />
+                        </Route>
+                    </Routes>
+                </ThemeProvider>
+            </BrowserRouter>
         </div>
     )
 }
